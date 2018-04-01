a326b70baeebea920236af5b71535963b7c9d186
PincelKEy <deckadecklantec@gmail.com>
2018-03-31 19:14:19 -0500

    Seccion ozono HTML

---
+++ pages/ozono.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php include_once "components/comp.meta.php" ?>
+    <title>Nosotros - OzonoRai</title>
+</head>
+<body>
+    <main class="con-principal">
+        <?php include_once "components/comp.header.php" ?>
+        <section class="cuerpo ozono">
+            <h1 class="ozono__title"><span>Ozono</span> y salud</h1>
+            <article class="ozono__descripcion">
+                <section class="descripcion__col-izq">
+                    <p class="descripcion__parrafo-simple">El OZONO (Oxígeno naciente) es el agente más oxidante (después del flúor) del que dispone el hombre. 
+                        Siendo además un decolorante muy efectivo y un potente destructor de gérmenes. 
+                        Mata bacterias y hongos con mayor rapidez que el cloro. </p>
+                    <p class="descripcion__parrafo-simple">El efecto deodorante del OZONO (O3) se debe a que destruye las sustancias de las cuales emanan los malos olores.</p>
+                    <p class="descripcion__parrafo-simple">Debido a las ventajosas propiedades del OZONO, se ha marcado un incremento en su utilización en 
+                        el mundo entero. Su utilización industrial en la purificación de aguas potables se conoce 
+                        desde hace más de 70 años (sin haberse registrado accidente alguno). </p>
+                    <div class="descripcion__parrafo-completo">
+                        <img src="<?php echo RUTA_IMG."ozono/ozosalud_img_01.png" ?>" alt="Limpieza de alimentos">
+                        <div class="parrafo-completo__contenido">
+                            <h2>Limpieza de los alimentos con agua ozonizada</h2>
+                            <p class="descripcion__parrafo-simple">El agua OZONIZADA, elimina las toxinas y olores desagradables de los alimentos (carnes, pescados, 
+                                frutas, verduras, etc...) Sin alterar sus valores nutritivos preservándolos por mayor tiempo. Eliminan los 
+                                olores que resultan al picar ajo, cebolla y otros. la preparación de los alimentos se hará con agua totalmente 
+                                pura.</p>
+                        </div>
+                    </div>
+                    <div class="descripcion__parrafo-completo"> 
+                        <img src="<?php echo RUTA_IMG."ozono/ozosalud_img_02.png" ?>" alt="Prevencion de la gastritis">
+                        <div class="parrafo-completo__contenido">
+                            <h2>Prevención de la gastritis y Cálculos con Agua Ozonizada</h2>
+                            <p class="descripcion__parrafo-simple">El agua OZONIZADA, tonifica las paredes estomacales previniendo la acidez, gastritis y úlceras, es muy útil 
+                                en la limpieza y cicatrización de pequeñas heridas. Regula la digestión y su uso cotidiano previene la 
+                                formación de caries y de cálculos renales.</p>
+                        </div>
+                    </div>
+                </section>
+                <section class="descripcion__col-der">
+                    <div class="descripcion__parrafo-completo">
+                        <img src="<?php echo RUTA_IMG."ozono/ozosalud_img_03.png" ?>" alt="Usos del ozono">
+                        <div class="parrafo-completo__contenido">
+                            <h2>USOS</h2>
+                            <p class="descripcion__parrafo-simple">Podemos comprobar como el OZONO EN EL TRATAMIENTO DEL AGUA ES UN GRAN SEGURO PARA EL CONSUMO DE LA MISMA EN 
+                                TODAS SUS VERSIONES.</p>
+                            <P class="descripcion__parrafo-simple">A continuación, daremos una relación (resumida) de usos de agua ozonizada</P>
+                            <ul class="contenido__lista">
+                                <li class="con__lista__item">En la bebida.</li>
+                                <li class="con__lista__item">Plantas embotelladas de agua mineral, refrescos o similares.</li>
+                                <li class="con__lista__item">En el higiene personal (beneficios en la piel).</li>
+                                <li class="con__lista__item">Para uso generalizado, en la vivienda.</li>
+                                <li class="con__lista__item">Limpieza de pescado.</li>
+                                <li class="con__lista__item">Limpieza de animales sacrificados.</li>
+                                <li class="con__lista__item">Limpieza de paredes y suelos locales.</li>
+                                <li class="con__lista__item">Limpieza de frutas, verduras, etc.</li>
+                                <li class="con__lista__item">Piscinas.</li>
+                                <li class="con__lista__item">Saunas de hidroterapia.</li>
+                                <li class="con__lista__item">Odontología.</li>
+                                <li class="con__lista__item">Piscinas de rehabilitación (medicina termal).</li>
+                                <li class="con__lista__item">Curas especiales en hospitales.</li>
+                                <li class="con__lista__item">Granjas.</li>
+                                <li class="con__lista__item">Limpieza de utensilios y recipientes utilizados por personas y animales.</li>
+                                <li class="con__lista__item">Eliminación de sabores y olores.</li>
+                                <li class="con__lista__item">Industrias de alimentación.</li>
+                                <li class="con__lista__item">Conservación del agua en cisternas de transporte.</li>
+                            </ul>
+                        </div>
+                    </div>                    
+                </section>
+            </article>
+        </section>
+        <?php include_once "components/comp.footer.php" ?>
+    </main>
+</body>
+</html>
