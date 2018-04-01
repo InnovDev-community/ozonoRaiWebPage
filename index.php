bebd2ce43a9afb28196f8da4ba602b7566ce560c
PincelKEy <deckadecklantec@gmail.com>
2018-04-01 03:55:46 -0500

    Seccion Producto HTML

--- index.php
+++ index.php
@@ -23,9 +23,11 @@
 	/*Analisis*/
 	/********************/
 	if(count($partes_ruta)==0+$cambio_local_remoto){
+
 		//Cuando no pedimos nada { ozonorai.net } 
 		$ruta_elegida='pages/inicio.php';
 	}else if(count($partes_ruta)==1+$cambio_local_remoto){
+
 		//Cuando pedimos una seccion { ozonorai.net/inicio/ }
 		switch ($partes_ruta[0+$cambio_local_remoto]) {
 			case 'inicio':
@@ -48,6 +50,14 @@
 				break;
 			case 'terminos_condiciones':
 				$ruta_elegida='pages/terminos_condiciones.php';
+				break;
+        }
+	}else if(count($partes_ruta)==3+$cambio_local_remoto){
+
+		//Cuando pedimos una seccion { ozonorai.net/productos/01/modelo_gota }
+		switch ($partes_ruta[0+$cambio_local_remoto]) {
+			case 'productos':
+				$ruta_elegida='pages/producto.php';
 				break;
         }
     }
--- pages/producto.php
+++ pages/producto.php
@@ -7,80 +7,35 @@
 <body>
     <main class="con-principal">
         <?php include_once "components/comp.header.php" ?>
-        <section class="cuerpo productos">
-            <h1 class="productos__title"><span>Modelos exclusivos</span> en Perú</h1>
-            <section class="productos__lista">
-                <div class="producto">
-                    <figure class="producto__imagen">
-                        <img src="<?php echo RUTA_IMG."productos/min/min_modelo_gota.png" ?>" alt="">
-                    </figure>
-                    <p class="producto__nombre">
-                        Modelo Gota en Acero Inoxidable
-                    </p>
+        <section class="cuerpo producto">
+            <section class="producto__contenedor">
+                <div>
+                    <img class="producto__imagen" src="<?php echo RUTA_IMG."productos/normal/modelo_gota.png" ?>" alt="Modelo Gota en Acero Inoxidable">
+                    <div class="producto__detalles">
+                        <h1>Modelo Gota en Acero Inoxidable</h1>
+                        <div>
+                            Hecho en Acero Inoxidable (18-8)
+                            Cañon Fijo.
+                            Medidas: 31 cm. x 21 cm. x 14cm.
+                        </div>
+                    </div>
                 </div>
-                <div class="producto">
-                    <figure class="producto__imagen">
-                        <img src="<?php echo RUTA_IMG."productos/min/min_modelo_alargado.png" ?>" alt="">
-                    </figure>
-                    <p class="producto__nombre">
-                        Modelo Alargado en Acero Inoxidable
-                    </p>
-                </div>
-                <div class="producto">
-                    <figure class="producto__imagen">
-                        <img src="<?php echo RUTA_IMG."productos/min/min_modelo_mini.png" ?>" alt="">
-                    </figure>
-                    <p class="producto__nombre">
-                        Modelo Mini en Acrílico - ABS
-                    </p>
-                </div>
-                <div class="producto">
-                    <figure class="producto__imagen">
-                        <img src="<?php echo RUTA_IMG."productos/min/min_modelo_aerodinamico.png" ?>" alt="">
-                    </figure>
-                    <p class="producto__nombre">
-                        Modelo Aerodinámico en Acrílico - ABS
-                    </p>
-                </div>
-                <div class="producto">
-                    <figure class="producto__imagen">
-                        <img src="<?php echo RUTA_IMG."productos/min/min_ducha_ozono.png" ?>" alt="">
-                    </figure>
-                    <p class="producto__nombre">
-                        Ducha aséptica con Ozono
-                    </p>
-                </div>
-                <div class="producto">
-                    <figure class="producto__imagen">
-                        <img src="<?php echo RUTA_IMG."productos/min/min_purificador_aire_domestico.png" ?>" alt="">
-                    </figure>
-                    <p class="producto__nombre">
-                        Purificador de Aire Doméstico
-                    </p>
-                </div>
-                <div class="producto">
-                    <figure class="producto__imagen">
-                        <img src="<?php echo RUTA_IMG."productos/min/min_purificador_agua_industrial.png" ?>" alt="">
-                    </figure>
-                    <p class="producto__nombre">
-                        Purificador de Agua Industrial
-                    </p>
-                </div>
+                <a class="pedido" href="<?php echo RUTA_CONTACTO ?>">
+                    Hacer <span>pedido</span>
+                </a>
             </section>
-            <section class="productos__extra">
-                <div class="otro_producto">
-                    <img src="<?php echo RUTA_IMG."productos/min/min_filtro_purificador.png" ?>" alt="">
-                    <p>Filtro Plus Americano</p>
+            <section class="producto__otros-productos">
+                <div class="otro-producto">
+                    <img src="<?php echo RUTA_IMG."productos/min/min_modelo_alargado.png" ?>" alt="Modelo Alargado en Acero Inoxidable">
+                    <p>Modelo Alargado en Acero Inoxidable</p>
                 </div>
-                <div class="informacion">
-                    <div class="info__1">
-                        <img src="<?php echo RUTA_IMG."productos/min/min_arrow_2.png" ?>" alt="Funcionamiento del sistema">
-                        <p>Funcionamiento del Sistema / Especifiaciones Técnicas</p>
-                    </div>
-                    <div class="info__2">
-                        <img src="<?php echo RUTA_IMG."productos/min/min_arrow_1.png" ?>" alt="Esquema de instalación">
-                        <p>Esquema de instalación</p>
-                    </div>
+                <div class="otro-producto">
+                    <img src="<?php echo RUTA_IMG."productos/min/min_modelo_mini.png" ?>" alt="Modelo Mini en Acrílico - ABS">
+                    <p>Modelo Mini en Acrílico - ABS</p>
+                </div>
+                <div class="otro-producto">
+                    <img src="<?php echo RUTA_IMG."productos/min/min_modelo_aerodinamico.png" ?>" alt="Modelo Aerodinámico en Acrílico - ABS">
+                    <p>Modelo Aerodinámico en Acrílico - ABS</p>
                 </div>
             </section>
         </section>
