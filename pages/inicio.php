581722fbcedd0709aa784396346fa7572241f21f
Martin <andy.pananar@gmail.com>
2018-04-02 15:09:40 -0500

    Slider de inicio funcional

--- pages/inicio.php
+++ pages/inicio.php
@@ -9,38 +9,42 @@
     <main class="con-principal">
         <?php include_once "components/comp.header.php" ?>
         <section class="cuerpo inicio">
-            <div class="inicio__slide">
-                <img class="slide__imagen" src="<?php echo RUTA_IMG."inicio/wall_vegetables.png" ?>" alt="Limpiez de verduras">
-                <div class="slide__descripcion">
-                    <p class="slide-des__title">elimina gérmenes y residuos químicos</p>
-                    <p class="slide-des__contenido">Ideal para la limpieza de frutas y verduras, carnes y pescados manteniendo por más tiempo
-                        su frescura</p>
-                </div>
-            </div>           
-            <div class="inicio__slide">
-                <img class="slide__imagen" src="<?php echo RUTA_IMG."inicio/wall_agua.png" ?>" alt="El agua es salud">
-                <div class="slide__descripcion">
-                    <p class="slide-des__title">salud & bienestar</p>
-                    <p class="slide-des__contenido">El ozono es el desinfectante natural más eficaz que se conoce</p>
-                </div>
-                <div class="slide__descripcion">
-                    <p slide-des__title>purificadores de agua a <span>base ozono</span> </p>
-                </div>
-            </div>           
-            <div class="inicio__slide">
-                <img class="slide__imagen-producto" src="<?php echo RUTA_IMG."inicio/pro01.png" ?>" alt="Modelo gota en acero inoxidable">
-                <img class="slide__imagen-producto" src="<?php echo RUTA_IMG."inicio/pro02.png" ?>" alt="Modelo aerodinámico en Acrílico - ABS">
-                <img class="slide__imagen-producto" src="<?php echo RUTA_IMG."inicio/pro03.png" ?>" alt="Filtro plus americano">
-                <img class="slide__imagen-producto" src="<?php echo RUTA_IMG."inicio/pro04.png" ?>" alt="Modelo alargado en acero inoxidable">
-                <img class="slide__imagen-producto" src="<?php echo RUTA_IMG."inicio/pro05.png" ?>" alt="Modelo mini eb acrílico - ABS">
-                <div class="slide__descripcion">
-                    <p class="slide-des__title">uso medicinal</p>
-                    <p class="slide-des__contenido">El agua ozonizada concentrada representa una gran ayuda en el tratamiento de 
-                        gastritis, ya que reduce la acidez estomacal y tonifica las paredes estomacales por 
-                        su defecto cicatrizante.
-                    </p>
-                </div>
-            </div>           
+            <figure class="slider">
+                <figure class="inicio__slide">
+                    <img class="slide__imagen" src="<?php echo RUTA_IMG."inicio/wall_vegetables.png" ?>" alt="Limpiez de verduras">
+                    <div class="slide__descripcion">
+                        <p class="slide-des__title">elimina gérmenes y residuos químicos</p>
+                        <p class="slide-des__contenido">Ideal para la limpieza de frutas y verduras, carnes y pescados manteniendo por más tiempo
+                            su frescura</p>
+                    </div>
+                </figure>           
+                <figure class="inicio__slide">
+                    <img class="slide__imagen" src="<?php echo RUTA_IMG."inicio/wall_agua.png" ?>" alt="El agua es salud">
+                    <div class="slide__descripcion">
+                        <p class="slide-des__title">salud & bienestar</p>
+                        <p class="slide-des__contenido">El ozono es el desinfectante natural más eficaz que se conoce</p>
+                    </div>
+                    <div class="slide__descripcion">
+                        <p slide-des__title>purificadores de agua a <span>base ozono</span> </p>
+                    </div>
+                </figure>           
+                <figure class="inicio__slide">
+                    <div class="slide__imagen">
+                        <img class="slide__imagen-producto" src="<?php echo RUTA_IMG."inicio/pro01.png" ?>" alt="Modelo gota en acero inoxidable">
+                        <img class="slide__imagen-producto" src="<?php echo RUTA_IMG."inicio/pro02.png" ?>" alt="Modelo aerodinámico en Acrílico - ABS">
+                        <img class="slide__imagen-producto" src="<?php echo RUTA_IMG."inicio/pro03.png" ?>" alt="Filtro plus americano">
+                        <img class="slide__imagen-producto" src="<?php echo RUTA_IMG."inicio/pro04.png" ?>" alt="Modelo alargado en acero inoxidable">
+                        <img class="slide__imagen-producto" src="<?php echo RUTA_IMG."inicio/pro05.png" ?>" alt="Modelo mini eb acrílico - ABS">
+                    </div>
+                    <div class="slide__descripcion">
+                        <p class="slide-des__title">uso medicinal</p>
+                        <p class="slide-des__contenido">El agua ozonizada concentrada representa una gran ayuda en el tratamiento de 
+                            gastritis, ya que reduce la acidez estomacal y tonifica las paredes estomacales por 
+                            su defecto cicatrizante.
+                        </p>
+                    </div>
+                </figure>   
+            </figure>        
         </section>
         <?php include_once "components/comp.footer.php" ?> -->
     </main>
