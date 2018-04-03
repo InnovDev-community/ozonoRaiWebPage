501ac6b5b2c583195028be082e983c971e3aff63
Martin <andy.pananar@gmail.com>
2018-04-02 23:26:00 -0500

    Slider mobile & tablet, Inicio casi completo

--- pages/inicio.php
+++ pages/inicio.php
@@ -23,9 +23,7 @@
                     <img class="slide__imagen" src="<?php echo RUTA_IMG."inicio/wall_agua.png" ?>" alt="El agua es salud">
                     <div class="slide__descripcion">
                         <p class="slide-des__title">Uso medicinal</p>
-                        <p class="slide-des__contenido">El agua ozonizada concentrada representa una gran ayuda en el tratamiento de 
-                            gastritis, ya que reduce la acidez estomacal y tonifica las paredes estomacales por 
-                            su defecto cicatrizante.
+                        <p class="slide-des__contenido">El agua ozonizada concentrada representa una gran ayuda en el tratamiento de gastritis, ya que reduce la acidez estomacal y tonifica las paredes estomacales por su defecto cicatrizante.
                         </p>
                     </div>
                 </figure>           
@@ -39,11 +37,11 @@
                             <img class="slide__imagen-producto-5" src="<?php echo RUTA_IMG."inicio/pro05.png" ?>" alt="Modelo mini eb acrílico - ABS">
                         </div>
                         <div class="slide__descripcion desc-1">
-                            <p class="slide-des__title">salud & bienestar</p>
+                            <p class="slide-des__title">Salud & bienestar</p>
                             <p class="slide-des__contenido">El ozono es el desinfectante natural más eficaz que se conoce</p>
                         </div>
                         <div class="slide__descripcion desc-2">
-                            <p class="slide-des__title">purificadores de agua a <span>base ozono</span> </p>
+                            <p class="slide-des__title">Purificadores de agua a <span>base ozono</span> </p>
                         </div>
                     </div>
                 </figure>   
