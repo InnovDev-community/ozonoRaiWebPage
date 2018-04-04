ea799c8504b1c99725545dc87b1f4e42045f80db
PincelKEy <deckadecklantec@gmail.com>
2018-04-03 20:12:16 -0500

    Seccion Funcionamiento, Especifiaciones y Esquema HTML

--- index.php
+++ index.php
@@ -50,9 +50,18 @@
 				break;
 			case 'terminos_condiciones':
 				$ruta_elegida='pages/terminos_condiciones.php';
+				break;			
+        }
+	}else if(count($partes_ruta)==2+$cambio_local_remoto){
+
+		//Cuando pedimos una seccion { ozonorai.net/productos/detalles_tecnicos }
+		switch ($partes_ruta[0+$cambio_local_remoto]) {
+			case 'productos':
+				$ruta_elegida='pages/detalles_tecnicos.php';
 				break;
         }
-	}else if(count($partes_ruta)==3+$cambio_local_remoto){
+    }	
+	else if(count($partes_ruta)==3+$cambio_local_remoto){
 
 		//Cuando pedimos una seccion { ozonorai.net/productos/01/modelo_gota }
 		switch ($partes_ruta[0+$cambio_local_remoto]) {
