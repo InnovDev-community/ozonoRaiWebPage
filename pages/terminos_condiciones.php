65db7b8dfd7d062e318c92955670998ae1ec5c37
PincelKEy <deckadecklantec@gmail.com>
2018-04-03 19:32:33 -0500

    Seccion faltante : Terminos y Condiciones HTML

---
+++ pages/terminos_condiciones.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php include_once "components/comp.meta.php" ?>
+    <title>Terminos y Condiciones - OzonoRai</title>
+</head>
+<body>
+    <main class="con-principal">
+        <?php include_once "components/comp.header.php" ?>
+        <section class="cuerpo terminos_condiciones">
+            <p>
+                Si decide visitar <span>www.ozonorai.net</span> debes de conocer las siguientes condiciones de 
+                uso; La primera de las cuales es tu aceptación, sin limitación o excepción, de los siguientes 
+                términos y condiciones.
+            </p>
+            <p>
+                1) Todos los logotipos y marcas en www.ozonorai.net está protegida como derecho de propiedad de 
+                DISTRIBUIDORA RAICES SAC y no podrás usarlas salvo permiso previo y expreso de DISTRIBUIDORA RAICES 
+                SAC 
+            </p>
+            <P>
+                2) Aunque DISTRIBUIDORA RAICES SAC realizó y realizará esfuerzos para incluir información precisa y 
+                actualizada en www.ozonorai.net, DISTRIBUIDORA RAICES no garantiza que dicha información sea absolutamente 
+                precisa. DISTRIBUIDORA RAICES no será responsable por ningún error u omisión contenidos en la página web y 
+                se reserva el derecho de modificarlo, y modificar estos términos y condiciones en cualquier momento y 
+                sin previo aviso.
+            </P>
+            <p>
+                3) La revisión y el uso de www.ozonorai.net es a tu exclusivo riesgo. Ni DISTRIBUIDORA RAICES ni nadie 
+                vinculado a la creación o desarrollo de la página web será responsable por ninguna clase de daños que pueda 
+                resultar de esa revisión y ese uso.
+            </p>
+            <p>
+                4) DISTRIBUIDORA RAICES SAC no se hará responsable de los contenidos publicados en otro/s sitio/s ajenos 
+                al entorno www.ozonorai.net - Te deseamos una agradable navegación.
+            </p>
+        </section>
+        <?php include_once "components/comp.footer.php" ?>
+    </main>
+</body>
+</html>
