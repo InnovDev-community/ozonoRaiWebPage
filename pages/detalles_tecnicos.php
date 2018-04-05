efa1b23071f67d5b7fdb1c0ba4d6934dfe58cd42
Martin <andy.pananar@gmail.com>
2018-04-05 12:03:19 -0500

    Vista detalles_tecnicos

--- pages/detalles_tecnicos.php
+++ pages/detalles_tecnicos.php
@@ -10,34 +10,32 @@
     <main class="con-principal">
         <?php include_once "components/comp.header.php" ?>
         <section class="cuerpo detalles_tecnicos">
-            <div>
+            <div class="detalles_tecnicos-izq">
                 <h1 class="detalles_tecnicos__title"><span>Funcionamiento </span> del sistema</h1>
                 <p> El sistema de pruficiación de agua realiza tres tratamiento: </p>
-                <ul>
-                    <li><span>Tratam. Fisico:</span> Retiene impurezas y sedimentos.</li>
-                    <li><span>Tratam. Químico:</span> Elimina el cloro, mal sabor y mal olor del agua.</li>
-                    <li><span>Tratam. Microbiológico:</span> Inyecta ozono para la desinfección y esterelización 
+                <ul class="lista-subtitulos">
+                    <li><span class="icon-angle-double-right"></span><span class="subrayado">Tratam. Fisico:</span> Retiene impurezas y sedimentos.</li>
+                    <li><span class="icon-angle-double-right"></span><span class="subrayado">Tratam. Químico:</span> Elimina el cloro, mal sabor y mal olor del agua.</li>
+                    <li><span class="icon-angle-double-right"></span><span class="subrayado">Tratam. Microbiológico:</span> Inyecta ozono para la desinfección y esterelización 
                     del agua, eliminando toda la población microbiológica.</li>
                 </ul>
 
-                <div>
+                <div class="especificaciones__tecnicas">
                     <h2><span>Especificaciones</span> técnicas</h2>
-                    <ul>
-                        <li>Voltaje de Entrada: 220v.</li>
-                        <li>Consumo de Energía: 25w.</li>
-                        <li>Capacidad Máxima de Agua: 180 Lts./h.</li>
-                        <li>Capacidad DE Ozonización: 25mg/h.</li>
-                        <li>Doble Sistema de Ozonización: Normal y Concentrada.</li>
+                    <ul class="lista-especificaciones">
+                        <li><span class="icon-angle-double-right"></span>Voltaje de Entrada: 220v.</li>
+                        <li><span class="icon-angle-double-right"></span>Consumo de Energía: 25w.</li>
+                        <li><span class="icon-angle-double-right"></span>Capacidad Máxima de Agua: 180 Lts./h.</li>
+                        <li><span class="icon-angle-double-right"></span>Capacidad DE Ozonización: 25mg/h.</li>
+                        <li><span class="icon-angle-double-right"></span>Doble Sistema de Ozonización: Normal y Concentrada.</li>
                     </ul>
                 </div>
             </div>
-            <div>
-                <div>
-                    <h2><span>Esquema de</span> instalación</h2>
-                    <figure>
-                        <img src="<?php echo RUTA_IMG."detalles_tecnicos/esquema_instalacion.png" ?>" alt="">
-                    </figure>
-                </div>           
+            <div class="detalles_tecnicos-der">
+                <h2><span>Esquema de</span> instalación</h2>
+                <figure>
+                    <img src="<?php echo RUTA_IMG."detalles_tecnicos/esquema_instalacion.png" ?>" alt="">
+                </figure>        
             </div>
         </section>
         <?php include_once "components/comp.footer.php" ?>
