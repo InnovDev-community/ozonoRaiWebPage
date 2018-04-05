bb6c4822fbd0af1a53171d452b9f335214fd9ff9
Martin <andy.pananar@gmail.com>
2018-04-04 23:48:27 -0500

    Modificaciones slider de inicio - vista pc

--- pages/inicio.php
+++ pages/inicio.php
@@ -14,16 +14,16 @@
                 <figure class="inicio__slide">
                     <img class="slide__imagen" src="<?php echo RUTA_IMG."inicio/wall_vegetables.png" ?>" alt="Limpiez de verduras">
                     <div class="slide__descripcion">
-                        <p class="slide-des__title">Elimina gérmenes y residuos químicos</p>
-                        <p class="slide-des__contenido">Ideal para la limpieza de frutas y verduras, carnes y pescados manteniendo por más tiempo
+                        <p class="slide-des__title titulo-1">elimina gérmenes <span>y residuos químicos<span></p>
+                        <p class="slide-des__contenido contenido-1">Ideal para la limpieza de frutas y verduras, carnes y pescados manteniendo por más tiempo
                             su frescura</p>
                     </div>
                 </figure>           
                 <figure class="inicio__slide">
                     <img class="slide__imagen prueba" src="<?php echo RUTA_IMG."inicio/wall_agua.png" ?>" alt="El agua es salud">
                     <div class="slide__descripcion">
-                        <p class="slide-des__title">Uso medicinal</p>
-                        <p class="slide-des__contenido">El agua ozonizada concentrada representa una gran ayuda en el tratamiento de gastritis, ya que reduce la acidez estomacal y tonifica las paredes estomacales por su defecto cicatrizante.
+                        <p class="slide-des__title titulo-2">uso <span>medicinal</span></p>
+                        <p class="slide-des__contenido contenido-2">El agua ozonizada concentrada representa una gran ayuda en el tratamiento de gastritis, ya que reduce la acidez estomacal y tonifica las paredes estomacales por su defecto cicatrizante.
                         </p>
                     </div>
                 </figure>           
@@ -37,11 +37,12 @@
                             <img class="slide__imagen-producto-5" src="<?php echo RUTA_IMG."inicio/pro05.png" ?>" alt="Modelo mini eb acrílico - ABS">
                         </div>
                         <div class="slide__descripcion desc-1">
-                            <p class="slide-des__title">Salud & bienestar</p>
-                            <p class="slide-des__contenido">El ozono es el desinfectante natural más eficaz que se conoce</p>
+                            <p class="slide-des__title">salud & <span>bienestar</span></p>
+                            <p class="slide-des__contenido">El ozono es el desinfectante</p>
+                            <p class="slide-des__contenido">natural más eficaz que se conoce</p>
                         </div>
                         <div class="slide__descripcion desc-2">
-                            <p class="slide-des__title">Purificadores de agua a <span>base ozono</span> </p>
+                            <p class="slide-des__title">purificadores de agua a <span>base ozono</span> </p>
                         </div>
                     </div>
                 </figure>   
